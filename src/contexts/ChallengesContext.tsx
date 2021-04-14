55d80cda91677e92aab1258ff9c4f11f40c99d27
Samuel Porto <samuelalcala2001@outlook.com>
2021-04-14 11:39:51 -0300

    Adicionando notificacoes

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from "react";
+import { createContext, useState, ReactNode, useEffect } from "react";
 import challenges from '../../challenges.json'
 
 // tipagem do objeto de desafios
@@ -38,6 +38,10 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   // caculo para o proximo nivel
   const experienceToNextLevel = Math.pow((level + 1) * 4 ,2)
 
+  useEffect(() => {
+    Notification.requestPermission()
+  }, [])
+
   // funcão para subir o nivel
   function levelUp() {
     setLevel(level + 1);
@@ -49,6 +53,14 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const challenge = challenges[randomChallengeIndex]
   
     setActiveChallenge(challenge)
+
+    new Audio('/notification.mp3').play()
+
+    if(Notification.permission === 'granted') {
+      new Notification ("Novo Desafio! 🎉", {
+        body: `Valendo ${challenge.amount}xp!`
+      })
+    }
   }
 
   // Função para resetar caso falhe no desafio.
